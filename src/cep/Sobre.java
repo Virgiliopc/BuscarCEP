49f5c552d54306554b0bdb6d35519630cb4486f5
Virgí­lio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-11 22:51:00 -0300

    update Sobre.java
    update Sobre.java

--- src/cep/Sobre.java
+++ src/cep/Sobre.java
@@ -5,16 +5,16 @@ import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.awt.SystemColor;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.URI;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class Sobre extends JDialog {
 
